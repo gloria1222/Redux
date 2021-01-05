9d6fa42b1d968d61ded2232cc5c0510dfa7f9b51
lcs930415 <lcs930415@gmail.com>
2021-01-05 14:51:53 -0500

    redux adv action-creator&async-actions

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,11 +1,11 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actions';
 
 const initialState = {
     counter: 0
-}
+};
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
+const reducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
         case actionTypes.INCREMENT:
             const newState = Object.assign({}, state);
             newState.counter = state.counter + 1;
@@ -18,12 +18,12 @@ const reducer = (state = initialState, action) => {
         case actionTypes.ADD:
             return {
                 ...state,
-                counter: state.counter + action.value
+                counter: state.counter + action.val
             }
         case actionTypes.SUBTRACT:
             return {
                 ...state,
-                counter: state.counter - action.value
+                counter: state.counter - action.val
             }
     }
     return state;
